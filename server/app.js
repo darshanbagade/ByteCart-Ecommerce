9b5574cdbd0168c42f4914fe1d6c39e500a6069b
Darshan Bagade <darshanbagade123@gmail.com>
2025-07-14 01:15:47 +0530

    Add cookie-parser middleware and user router integration

--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import cors from "cors"
-
+import cookieParser from 'cookie-parser';
 const app = express()
 
 app.use(cors({
     origin: ['http://localhost:3000'],
     credentials:true
 }));
+
 
 app.use(express.json({
     limit:'16kb'
@@ -17,6 +18,11 @@ app.use(express.json({
 //When someone visits http://yoursite.com/style.css, 
 //Express will look for public/style.css and serve it if found.
 app.use(express.static("public"))
+
+app.use(cookieParser());
+
+import { userRouter } from './routes/user.route.js';
+app.use('/api/v1/user',userRouter)
 
 
 export {app}
