43daa1043e34c3821e5c96eb1d4c695bb9053143
Darshan Bagade <darshanbagade123@gmail.com>
2025-07-14 00:18:24 +0530

    Add ApiError class for custom error handling

---
+++ server/utils/ApiError.js
@@ -0,0 +1,23 @@
+// Modifing the inbuilt Error function of nde by inheritence
+class ApiError extends Error{
+    constructor(
+        statusCode,
+        message = "Something went wrong",
+        errors = [],
+        stack = "" 
+    ){
+        super(message)
+        this.statusCode = statusCode,
+        this.data = null,
+        this.message = message,
+        this.success = false,
+        this.errors = errors
+        if(stack){
+            this.stack = stack
+        }else{
+            Error.captureStackTrace(this , this.constuctor)
+        }
+    }
+}
+
+export { ApiError }
