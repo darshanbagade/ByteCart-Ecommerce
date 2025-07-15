85c7fea6f8fba2303c950049c2fda5f90d9cfa9e
Darshan Bagade <darshanbagade123@gmail.com>
2025-07-16 00:22:11 +0530

    Add ApiResponse utility class for standardized API responses

---
+++ server/utils/ApiResponse.js
@@ -0,0 +1,14 @@
+class ApiResponce {
+    constructor(
+        statusCode,
+        data,
+        message = "success"
+    ){
+        this.statusCode = statusCode,
+        this.message = message,
+        this.data = data,
+        this.success = statusCode < 400
+    }
+}
+
+export {ApiResponce};
