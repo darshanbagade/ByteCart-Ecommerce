70132dd9248208ad761c0bc3f0e9e32847829b19
Darshan Bagade <darshanbagade123@gmail.com>
2025-08-03 10:42:25 +0530

    Add index.js to export main components for easier imports

---
+++ client/src/components/index.js
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar";
+import { Hero } from "./Hero";
+import { CategoryMenu } from "./CategoryMenu";
+import { ProductCard } from "./ProductCard";
+import {ProductList} from "./ProductList";
+import Footer from "./Footer";
+export {
+    Navbar,
+    Hero,
+    CategoryMenu,
+    ProductCard,
+    ProductList,
+    Footer    
+}
