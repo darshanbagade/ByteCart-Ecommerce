63aa6a1e1d8c1fa39096b36404f932c599c41283
Darshan Bagade <darshanbagade123@gmail.com>
2025-08-03 10:41:22 +0530

    Add Home component to structure main page layout with Navbar, Hero, CategoryMenu, ProductList, and Footer

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Navbar,Hero, CategoryMenu, ProductList, Footer } from '../components/index.js'
+function Home() {
+  return (
+    <div className='px-2 md:px-32 lg:px-32'>
+        <Navbar/>
+        <Hero/>
+        <CategoryMenu/>
+        <ProductList/>
+        <Footer/>
+        
+    </div>
+  )
+}
+
+export default Home
