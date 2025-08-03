b0c047ca4fa23a9cc0bea20de6298de97a3da123
Darshan Bagade <darshanbagade123@gmail.com>
2025-08-03 13:56:04 +0530

    Add additional iPhone asset images for enhanced product display

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,7 +1,12 @@
 import Iphone_01 from './Iphone_01.png'
 import Iphone_02 from './Iphone_02.png'
+import Iphone_03 from './Iphone_03.png'
+import Iphone_04 from './Iphone_04.png'
+
 export{
     Iphone_01,
-    Iphone_02
+    Iphone_02,
+    Iphone_03,
+    Iphone_04
 
 }
