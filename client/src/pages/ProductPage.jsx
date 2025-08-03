2c959803acfa864c94ba29ea9a00045608668ed6
Darshan Bagade <darshanbagade123@gmail.com>
2025-08-03 13:56:31 +0530

    Add ProductPage component for displaying product details and related items

---
+++ client/src/pages/ProductPage.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Footer, Navbar, ProductDetails, ProductList } from '../components/index';
+import { Iphone_01, Iphone_02, Iphone_03, Iphone_04} from '../assets/index';
+import { useParams } from 'react-router-dom';
+
+const ProductPage = () => {
+
+    const {slug} = useParams(); 
+    const product = {
+    _id: '1',
+    title: 'Iphone 14 Pro',
+    slug: 'iphone-15-pro',
+    description:
+      'The iPhone 14 Pro features a 6.1-inch Super Retina XDR display, A16 Bionic chip, and advanced triple-camera system.',
+    price: 200000,
+    images: [Iphone_01, Iphone_02, Iphone_03, Iphone_04], 
+    stock: 10,
+    brand: 'Apple',
+    features: ['A16 Bionic Chip', 'Triple Camera System', 'Super Retina XDR Display', '5G Support'],
+    category: 'Mobile',
+    ratingAvg: 4.5,
+    reviews: [],
+  };
+
+
+  return (
+    <div className='px-2 md:px-32 lg:px-32'>
+      <Navbar/>
+      <ProductDetails product={product}/>
+      <ProductList category={product.category} productRange={8}/>
+      <Footer/>
+    </div>
+  )
+}
+
+export  {ProductPage};
