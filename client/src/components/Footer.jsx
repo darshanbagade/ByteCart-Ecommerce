b6c2d49b3f46da5d74272886b1a75a27ce065fe9
Darshan Bagade <darshanbagade123@gmail.com>
2025-08-03 13:55:52 +0530

    Enhance layout of Footer and Hero components for improved responsiveness and visual appeal

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className="bg-white border-2 rounded-2xl my-6 text-black py-8">
+    <footer className="container mx-auto bg-white border-2 rounded-2xl my-6 text-black py-8">
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {/* About Section */}
--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -3,17 +3,17 @@ import { Link } from 'react-router-dom';
 
 export const Hero = () => {
   return (
-    <section className="bg-gradient-to- from-black via-white to-white text-white py-16 md:py-24 my-8  rounded-4xl border-2 border-black ">
+    <section className="container mx-auto bg-gradient-to- from-black via-white to-white text-white py-8 md:py-16 lg:py-16 lg:my-8 mg:my-8  rounded-4xl border-2 border-black ">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col md:flex-row items-center">
-        {/* Text Content */}
+       
         <div className="md:w-1/2 text-center md:text-left mb-8 md:mb-0">
-          {/* Headline */}
+          
           <h1 className="text-3xl text-black sm:text-5xl font-extrabold leading-tight mb-4">
             Upgrade Your Tech Game
           </h1>
 
-          {/* Subheading */}
-          <p className="text-base sm:text-lg mb-6 max-w-md mx-auto md:mx-0 text-black">
+         
+          <p className="lg:text-base text-sm md:text-base sm:text-lg  md:mb-6 lg:mb-6 max-w-md mx-auto md:mx-0 text-black">
             Explore the latest smartphones, laptops, and accessories at unbeatable prices. Innovate your life today!
           </p>
 
@@ -26,14 +26,14 @@ export const Hero = () => {
           </Link> */}
         </div>
 
-        {/* Image Placeholder */}
-        <div className="md:w-1/2">
+        
+        <div className="md:w-1/2 ">
           <div className="relative">
-            {/* Placeholder for electronics image */}
-            <div className="w-full h-64 sm:h-80 bg-gray-600 rounded-lg flex items-center justify-center text-gray-300">
-              <span className="text-lg">Latest Smartphone & Laptop</span>
-            </div>
-            {/* Decorative Element */}
+        
+            <div className="w-full h-64 sm:h-80 px-14 bg-gray-600 rounded-lg flex items-center justify-center text-gray-300">
+              <span className="text-lg text-center">Latest Smartphone <br /> & Laptop</span>
+            </div>  
+          
             <div className="absolute -bottom-4 -left-4 w-20 h-20 bg-black rounded-full opacity-50"></div>
           </div>
         </div>
