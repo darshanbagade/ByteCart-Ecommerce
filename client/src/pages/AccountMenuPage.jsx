7bb96800102d62b6c23c1a1f5e3b56e30adf2a80
Darshan Bagade <darshanbagade123@gmail.com>
2025-08-07 23:50:52 +0530

    Add UserMenu and AccountPage components for user account navigation

---
+++ client/src/pages/AccountMenuPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Navbar, Footer, MenuButton } from '../components/index'
+
+function UserMenu() {
+  return (
+    <div >
+        <Navbar/>
+        <MenuButton/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default UserMenu
---
+++ client/src/pages/AccountPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Navbar,Footer } from '../components'
+function AccountPage() {
+  return (
+    <div>
+        <Navbar/>
+        Account Page
+        <Footer/>
+    </div>
+  )
+}
+
+export default AccountPage
