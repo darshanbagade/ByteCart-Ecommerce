ccc42492f7afe65d0c8756e0b2ea2a1fdd2cd4eb
Darshan Bagade <darshanbagade123@gmail.com>
2025-08-07 23:51:27 +0530

    Add admin components and update imports in index files for improved structure

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -6,13 +6,65 @@ import {ProductList} from "./ProductList";
 import Footer from "./Footer";
 
 import { ProductDetails } from "./ProductDetails";
+import MenuButton from "./account/MenuButton";
+import CartList from "./cart/CartList";
+
+import OrderCard from "./order/OrderCard";
+import OrderList from "./order/OrderList";
+import Checkout from "./cart/Checkout";
+
+//Admin-Dashboard
+import DashboardMenu from "./admin/DashboardMenu";
+import AdminDashboard from "./admin/AdminDashboard";
+
+//Admin-Order
+import AdminOrders from "./admin/orders/AdminOrders";
+import AdminOrderCard from "./admin/orders/AdminOrderCard";
+
+//Admini-product
+import AdminProducts from "./admin/product/AdminProducts";
+import AdminProductCard from "./admin/product/AdminProductCard";
+import AddProduct from "./admin/product/AddProduct";
+import EditProduct from "./admin/product/EditProduct";
+
+//Admin-Category
+import EditCategory from "./admin/category/EditCategory";
+import AddCategory from "./admin/category/AddCategory";
+import AdminCategories from "./admin/category/AdminCategories";
+
 
 export {
     Navbar,
     Hero,
     CategoryMenu,
+    
     ProductCard,
     ProductList,
     Footer    ,
-    ProductDetails
+
+    MenuButton,
+    ProductDetails,
+    CartList,
+    Checkout,
+
+    OrderCard,
+    OrderList,
+
+    DashboardMenu,
+    AdminOrderCard,
+    AdminOrders,
+    AdminProductCard,
+    AdminProducts,
+    AddProduct,
+    EditProduct,
+    AdminDashboard,
+
+    //category
+    AdminCategories,
+    AddCategory,
+    EditCategory
+
+
+    
+    
 }
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,7 +1,29 @@
 import Home from "./Home";
 import { ProductPage } from "./ProductPage";
+import Cart from "./Cart";
+import CartLayout from "./CartLayout";
+import AccountMenuPage from './AccountMenuPage'
+import OrderPage from "./OrderPage";
+import AccountPage from "./AccountPage";
 
+import Layout from "./admin/Layout";
+import Dashboard from "./admin/Dashboard";
+import Orders from "./admin/Orders";
+import Category from "./admin/Category";
+import Product from "./admin/Product";
 export{
     Home,
-    ProductPage
+    ProductPage,
+    Cart,
+    CartLayout,
+    AccountMenuPage,
+    OrderPage,
+    AccountPage,
+
+    Layout,
+    Dashboard,
+    Orders,
+    Category,
+    Product
+
 }
