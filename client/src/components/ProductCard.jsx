5afc63a239b23ea31df31a4a95c0323d5ba6a066
Darshan Bagade <darshanbagade123@gmail.com>
2025-08-10 00:13:36 +0530

    Refactor ProductCard and ProductList components for improved structure and data handling

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,32 +1,32 @@
-/// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 import { Link, Links } from "react-router-dom";
 
 export const ProductCard = ({ product }) => {
   return (
    <Link to={`/${product.slug}`}>
-       <div className="border-2 lg:w-55.5 rounded-2xl bg-white transition-transform transform hover:-translate-y-2">
+       <div className="border-2 rounded-2xl bg-white transition-transform transform hover:-translate-y-2 w-full max-w-sm mx-auto">
       <img
-        src={product.image || 'https://via.placeholder.com/200'}
-        alt={product.name}
-        className="w-full lg:h-48 md:h-48 h-38 object-cover lg:mb-4 md:mb-4 rounded-t-[14px] "
+        src={product.images[0] || 'https://via.placeholder.com/200'}
+        alt={product.title}
+        className="w-full h-32 sm:h-40 md:h-44 lg:h-48 object-cover rounded-t-[14px]"
       />
-      <div className=" px-4 mt-2 md:p-4 lg:p-4 ">
-        <h3 className="text-lg  md:font-semibold lg:font-semibold text-gray-800 truncate">{product.name}</h3>
-        <p className="text-sm text-gray-600">{product.category}</p>
-        <p className="lg:text-xl pb-4 md:text-xl text-sm font-bold text-black mt-2">${product.price}</p>
+      <div className="p-2 sm:p-3 md:p-4">
+        <h3 className="text-sm sm:text-base md:text-lg font-medium md:font-semibold text-gray-800 truncate">{product.title}</h3>
+        <p className="text-xs sm:text-sm text-gray-600">{product.category.name}</p>
+        <p className="text-sm sm:text-lg md:text-xl font-bold text-black mt-1 md:mt-2 pb-2 md:pb-4">${product.price}</p>
       </div>
     </div>
    </Link>
   );
 };
 
-// ProductCard.propTypes = {
-//   product: PropTypes.shape({
-//     _id: PropTypes.string.isRequired,
-//     name: PropTypes.string.isRequired,
-//     category: PropTypes.string.isRequired,
-//     price: PropTypes.number.isRequired,
-//     image: PropTypes.string,
-//   }).isRequired,
-// };
+ProductCard.propTypes = {
+  product: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    image: PropTypes.string,
+  }).isRequired,
+};
--- client/src/components/ProductList.jsx
+++ client/src/components/ProductList.jsx
@@ -1,147 +1,61 @@
-// src/components/ProductList.jsx
-import React from 'react';
-import { ProductCard } from '../components/index';
+import {useState, useEffect} from 'react';
 import { Iphone_01, Iphone_02 } from '../assets/index';
+import { getAllProducts } from '../services/productServices';
+import { ProductCard } from '../components/index';
+import {Loader} from '../components/index';
 
-const ProductList = ({
-  category = "",
-  productRange = 4
+const ProductList = (
+  {
+    category = "Mobile",
+    productRange = 4
+  }) => {
 
-}) => {
-  const products = [
-    {
-      _id: 1,
-      name: 'Iphone',
-      slug:"iphone-15-pro",
-      price: 200000,
-      category: 'Mobile',
-      image: Iphone_01,
-    },
-    {
-      _id: 2,
-      name: 'Iphone',
-      slug:"iphone-14-pro",
-      price: 200000,
-      category: 'Mobile',
-      image: Iphone_02,
-    },
-    {
-      _id: 3,
-      name: 'Iphone',
-      slug:"iphone-14-pro",
-      price: 200000,
-      category: 'Mobile',
-      image: Iphone_01,
-    },
-    {
-      _id: 4,
-      name: 'Iphone',
-      slug:"iphone-14-pro",
-      price: 200000,
-      category: 'Mobile',
-      image: Iphone_02,
-    },
-    {
-      _id: 5,
-      name: 'Iphone',
-      slug:"iphone-14-pro",
-      price: 200000,
-      category: 'Mobile',
-      image: Iphone_01,
-    },
-    {
-      _id: 7,
-      name: 'HP',
-      slug:"iphone-14-pro",
-      price: 40000,
-      category: 'Laptop',
-      image: Iphone_01,
-    },
-    {
-      _id: 8,
-      name: 'Iphone',
-      slug:"iphone-14-pro",
-      price: 90000,
-      category: 'Laptop',
-      image: Iphone_02,
-    },
-    {
-      _id: 9,
-      name: 'HP',
-      price: 40000,
-      slug:"iphone-14-pro",
-      category: 'Laptop',
-      image: Iphone_01,
-    },
-    {
-      _id: 10,
-      name: 'HP',
-      slug:"iphone-14-pro",
-      price: 40000,
-      category: 'Laptop',
-      image: Iphone_01,
-    },
-    {
-      _id: 11,
-      name: 'Iphone',
-      slug:"iphone-14-pro",
-      price: 90000,
-      category: 'Headphones',
-      image: Iphone_02,
-    },
-    {
-      _id: 12,
-      name: 'Iphone',
-      slug:"iphone-14-pro",
-      price: 90000,
-      category: 'Headphones',
-      image: Iphone_02,
-    },
-    {
-      _id: 13,
-      name: 'Iphone',
-      slug:"iphone-14-pro",
-      price: 90000,
-      category: 'Headphones',
-      image: Iphone_02,
-    },
-    {
-      _id: 14,
-      name: 'Iphone',
-      slug:"iphone-14-pro",
-      price: 90000,
-      category: 'Headphones',
-      image: Iphone_02,
-    },
-    {
-      _id: 15,
-      name: 'Iphone',
-      slug:"iphone-14-pro",
-      price: 90000,
-      category: 'Headphones',
-      image: Iphone_02,
-    },
-  ];
 
+  const [products, setProducts ] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(()=>{
+    const fetchProducts = async() => {
+      setLoading(true);
+      setError(null);
+      try{
+        const response = await getAllProducts();
+        setProducts(response.data.data);
+        console.log(response.data.data)
+      } catch(err){
+        setError("Failed to fetch te data");
+      } finally{
+        setLoading(false);
+      }
+    }
+    fetchProducts();
+  },[])
+
+
+
+
+  
   return (
     <div className="my-10">
       {/* Mobiles Section */}
       <div className="my-4 text-xl font-semibold">{category}</div>
       <div>
-        {products.length > 0 ? (
-          <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-11 lg:gap-11 ">
+        {loading && <Loader/>}
+        {error && <p className="text-center text-red-500">{error}</p>}
+        {!loading && !error && products.length > 0 ? (
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 lg:gap-8">
             {products
-              .filter((product) => product.category === category)
+              .filter((product) => product.category.name === category)
               .slice(0, productRange)
               .map((product) => (
                 <ProductCard key={product._id} product={product} />
               ))}
           </div>
         ) : (
-          <p className="text-gray-600 text-center">No products found.</p>
+          !loading && !error && <p className="text-gray-600 text-center">No products found.</p>
         )}
       </div>
-        
     </div>
 
 
