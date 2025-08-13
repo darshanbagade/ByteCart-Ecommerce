bb118fea40cfc6d337bdeaf66bb1d8d3b986a45b
Darshan Bagade <darshanbagade123@gmail.com>
2025-08-13 23:53:35 +0530

    Update Loader component styles for improved layout and responsiveness

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Loader = () => {
   return (
-    <div className="flex justify-center items-center py-8">
+    <div className="flex justify-center items-center h-screen mx-[50] ">
       <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900"></div>
     </div>
   );
