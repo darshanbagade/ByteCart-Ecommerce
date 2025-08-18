1de8021be0cdd68478d2357df8a2186ceb2476d0
Darshan Bagade <darshanbagade123@gmail.com>
2025-08-19 01:13:43 +0530

    Fix typos and improve formatting in ApiError class definition

--- server/utils/ApiError.js
+++ server/utils/ApiError.js
@@ -1,23 +1,23 @@
-// Modifing the inbuilt Error function of nde by inheritence
-class ApiError extends Error{
+// Modifying the inbuilt Error function of Node by inheritance
+class ApiError extends Error {
     constructor(
         statusCode,
         message = "Something went wrong",
         errors = [],
-        stack = "" 
-    ){
-        super(message)
-        this.statusCode = statusCode,
-        this.data = null,
-        this.message = message,
-        this.success = false,
-        this.errors = errors
-        if(stack){
-            this.stack = stack
-        }else{
-            Error.captureStackTrace(this , this.constructor)
+        stack = ""
+    ) {
+        super(message);
+        this.statusCode = statusCode;
+        this.data = null;
+        this.message = message;
+        this.success = false;
+        this.errors = errors;
+        if (stack) {
+            this.stack = stack;
+        } else {
+            Error.captureStackTrace(this, this.constructor);
         }
     }
 }
 
-export { ApiError }
+export { ApiError };
