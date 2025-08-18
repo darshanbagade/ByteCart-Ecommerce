8e412e6a0cef56c1f4aeaeb55f0af581bed862e1
Darshan Bagade <darshanbagade123@gmail.com>
2025-08-19 01:13:50 +0530

    Add userSignup and userLogout functions to authServices

--- client/src/services/authServices.js
+++ client/src/services/authServices.js
@@ -1,5 +1,12 @@
 import api from "./api";
 
+const userSignup = async (payload) =>{
+    return await api.post('/api/v1/user/register',payload)
+    // console.log(res)
+
+    
+}
+
 const userLogin = async(payload)=>{
     return await api.post('/api/v1/user/login',payload)
     // console.log(res)
@@ -9,7 +16,15 @@ const userLogin = async(payload)=>{
 const getCurrentUser = async ()=>{
     return await api.get('/api/v1/user/current-user')
 }
+
+const userLogout = async ()=>{
+    return await api.post('/api/v1/user/logout')
+}
+
 export {
+    userSignup,
     userLogin,
-    getCurrentUser
+    getCurrentUser,
+    userLogout,
+    
 };
